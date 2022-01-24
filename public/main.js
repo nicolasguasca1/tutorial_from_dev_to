23efca793d27d1e281da34a79069e4e583a1a645
nicolasguasca1 <nicolas.guasca@gmail.com>
2022-01-24 19:41:03 +0100

    Part 3 finalized

--- public/main.js
+++ public/main.js
@@ -1,10 +1,8 @@
-const { on } = require("events");
-
 const localVideo = document.getElementById("localVideo");
 const remoteVideo = document.getElementById("remoteVideo");
 const callButton = document.getElementById("callButton");
 const hangupButton = document.getElementById("hangupButton");
-const socket = new WebSocket("ws://localhost:8888");
+const socket = new WebSocket("wss://localhost:8888");
 
 let peerConnection;
 let localMediaStream;
@@ -12,161 +10,95 @@ let remoteId;
 const remoteMediaStream = new MediaStream();
 
 socket.onopen = () => {
-    console.log("socket::open");
+  console.log("socket::open");
 };
 
-socket.onmessage = async ({data}) => {
-    try {
-        const jsonMessage = JSON.parse(data);
-        console.log('action', jsonMessage.action);
-        switch (jsonMessage.action) {
-            case "start":
-                console.log("start", jsonMessage.id);
-                callButton.disabled = false;
+socket.onmessage = async ({ data }) => {
+  try {
+    const jsonMessage = JSON.parse(data);
 
-                document.getElementById('localId').innerHTML = jsonMessage.id;
-                break;
-            case "offer":
-                remoteId = jsonMessage.data.remoteId;
-                delete jsonMessage.data.remoteId;
+    console.log("action", jsonMessage.action);
+    switch (jsonMessage.action) {
+      case "start":
+        console.log("start", jsonMessage.id);
+        callButton.disabled = false;
+        document.getElementById("localId").innerHTML = jsonMessage.id;
+        break;
+      case "offer":
+        remoteId = jsonMessage.data.remoteId;
+        delete jsonMessage.data.remoteId;
 
-                await initializePeerConnection(localMediaStream.getTracks());
-                await peerConnection.setRemoteDescription(new RTCSessionDescription(jsonMessage.data.offer));
+        await initializePeerConnection(localMediaStream.getTracks());
+        await peerConnection.setRemoteDescription(
+          new RTCSessionDescription(jsonMessage.data.offer)
+        );
 
-                const answer = await peerConnection.createAnswer();
-                await peerConnection.setLocalDescription(answer);
+        const answer = await peerConnection.createAnswer();
+        await peerConnection.setLocalDescription(answer);
 
-                sendSocketMessage('answer', {remoteId, answer});
-                break;
-            case "answer":
-                await peerConnection.setRemoteDescription(new RTCSessionDescription(jsonMessage.data.answer));
-                break;
-            case "ideCandidate":
-                await peerConnection.addIceCandidate(jsonMessage.data.Candidate);
-                break;
-            default: console.log('unknown action', jsonMessage.action);
-        }
-    } catch (error) {
-        console.error("failed to handle socket message", error);
+        sendSocketMessage("answer", { remoteId, answer });
+        break;
+      case "answer":
+        await peerConnection.setRemoteDescription(
+          new RTCSessionDescription(jsonMessage.data.answer)
+        );
+        break;
+      case "iceCandidate":
+        await peerConnection.addIceCandidate(jsonMessage.data.candidate);
+        break;
+      default:
+        console.warn("unknown action", jsonMessage.action);
     }
+  } catch (error) {
+    console.error("failed to handle socket message", error);
+  }
 };
 
 socket.onerror = (error) => {
-    console.error("socket::error", error);
+  console.error("socket::error", error);
 };
 
 socket.onclose = () => {
-    console.log("socket::close");
-    stop();
+  console.log("socket::close");
+  stop();
 };
 
 const sendSocketMessage = (action, data) => {
-    const message = {action, data};
-    socket.send(JSON.stringify(message));
+  const message = { action, data };
+  socket.send(JSON.stringify(message));
 };
 
 const start = async () => {
-    try {
-        localMediaStream = await getLocalMediaStream();
-        sendSocketMessage('start');
-    } catch (error) {
-        console.error("failed to start stream", error);
-    }
+  try {
+    localMediaStream = await getLocalMediaStream();
+
+    sendSocketMessage("start");
+  } catch (error) {
+    console.error("failed to start stream", error);
+  }
 };
 
 const call = async () => {
-    try {
-        remoteId = document.getElementById('remoteId').value;
+  try {
+    remoteId = document.getElementById("callId").value;
 
-        if(!remoteId) {
-            alert('Please enter a remoteId');
-            return;
-        }
+    if (!remoteId) {
+      alert("Please enter a remote id");
 
-        console.log('call', remoteId);
-        await initializePeerConnection(localMediaStream.getTracks());
-        const offer = await peerConnection.createOffer();
-        await peerConnection.setLocalDescription(offer);
-        sendSocketMessage('offer', {offer, remoteId});
-    } catch (error) {
-        console.error("failed to initialize call", error);
+      return;
     }
+
+    console.log("call: ", remoteId);
+    await initializePeerConnection(localMediaStream.getTracks());
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    sendSocketMessage("offer", { offer, remoteId });
+  } catch (error) {
+    console.error("failed to initialize call", error);
+  }
 };
 
 const hangup = () => socket.close();
-
-const stop = () => {
-    if (!localVideo.srcObject) return;
-
-    for (const track of localVideo.srcObject.getTracks()) {
-        track.stop();
-    }
-
-    peerConnection.close();
-    callButton.disabled = true;
-    hangupButton.disabled = true;
-    localVideo.srcObject = undefined;
-    remoteVideo.srcObject = undefined;
-};
-
-const getLocalMediaStream = async () => {
-    try {
-        const mediaStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true
-        });
-        console.log('got local media stream', mediaStream);
-
-        localVideo.srcObject = mediaStream;
-        return mediaStream;
-    }
-    catch (error) {
-        console.error("failed to get local media stream", error);
-    }
-};
-
-const initializePeerConnection = async (mediaTracks) => {
-    const config = { iceServers: [{ urls: ["stun:stun.l.google.com:19302"] }] };
-    peerConnection = new RTCPeerConnection(config);
-
-    peerConnection.onicecandidate = ({ candidate }) => {
-        if (!candidate) return;
-        console.log('peerConnection::icecandidate', candidate);
-        console.log('remote', remoteId);
-        console.log('iceCandidate', { remoteId, candidate});
-    };
-
-    peerConnection.oniceconnectionstatechange = () => {
-        console.log('peerConnection::iceconnectionstatechange newState=', peerConnection.iceConnectionState);
-        if (peerConnection.iceConnectionState === "disconnected") {
-            alert('Connection has been closed. Stopping...');
-            socket.close();
-        }
-    };
-
-    peerConnection.ontrack = ({ track }) => {
-        console.log('peerConnection::track', track);
-        remoteMediaStream.addTrack(track);
-        remoteVideo.srcObject = remoteMediaStream;
-    };
-
-    for (const track of mediaTracks) {
-        peerConnection.addTrack(track);
-    }
-};
-
-hangupButton.disabled = false;
-
-const startDefault = () => getMedia({ video: true, audio: true });
-
-const startVGA = () =>
-  getMedia({ video: { width: 640, height: 480 }, audio: true });
-
-const startHD = () =>
-  getMedia({ video: { width: 1280, height: 720 }, audio: true });
-
-const startFullHD = () =>
-  getMedia({ video: { width: { min: 600, max: 1300 }, height: { min: 300, max: 800 } } };
 
 const stop = () => {
   if (!localVideo.srcObject) return;
@@ -174,15 +106,63 @@ const stop = () => {
   for (const track of localVideo.srcObject.getTracks()) {
     track.stop();
   }
+
+  peerConnection.close();
+  callButton.disabled = true;
+  hangupButton.disabled = true;
+  localVideo.srcObject = undefined;
+  remoteVideo.srcObject = undefined;
 };
 
-const getMedia = async (constraints) => {
+const getLocalMediaStream = async () => {
   try {
-    console.log("getMedia constraints: ", constraints);
-    const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    console.log("got local media stream");
+
     localVideo.srcObject = mediaStream;
+
+    return mediaStream;
   } catch (error) {
-    alert("failed to get media devices, see console for error");
-    console.error(error);
+    console.error("failed to get local media stream", error);
   }
+};
+
+const initializePeerConnection = async (mediaTracks) => {
+  const config = { iceServers: [{ urls: ["stun:stun1.l.google.com:19302"] }] };
+  peerConnection = new RTCPeerConnection(config);
+
+  peerConnection.onicecandidate = ({ candidate }) => {
+    if (!candidate) return;
+
+    console.log("peerConnection::icecandidate", candidate);
+    console.log("remote", remoteId);
+    sendSocketMessage("iceCandidate", { remoteId, candidate });
+  };
+
+  peerConnection.oniceconnectionstatechange = () => {
+    console.log(
+      "peerConnection::iceconnectionstatechange newState=",
+      peerConnection.iceConnectionState
+    );
+    // If ICE state is disconnected stop
+    if (peerConnection.iceConnectionState === "disconnected") {
+      alert("Connection has been closed stopping...");
+      socket.close();
+    }
+  };
+
+  peerConnection.ontrack = ({ track }) => {
+    console.log("peerConnection::track", track);
+    remoteMediaStream.addTrack(track);
+    remoteVideo.srcObject = remoteMediaStream;
+  };
+
+  for (const track of mediaTracks) {
+    peerConnection.addTrack(track);
+  }
+
+  hangupButton.disabled = false;
 };
